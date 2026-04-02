448fb68e6f1e2fd8bfe8112dd31a77135cf53fb3
Aviv Keller <me@aviv.sh>
2026-04-02 13:53:41 -0400

    feat(deployments): deploy to `/` (#13)
    Signed-off-by: Aviv Keller <me@aviv.sh>

--- doc-kit.config.mjs
+++ doc-kit.config.mjs
@@ -4,7 +4,7 @@ import { join } from 'node:path';
 /** @type {import('@node-core/doc-kit/src/utils/configuration/types.d.ts').Configuration} */
 export default {
   global: {
-    output: 'out/learn',
+    output: 'out',
     input: ['pages/**/*.md'],
   },
   'jsx-ast': {
