d1a4922162a7253091a89120459d9b93d03491bf
Aviv Keller <me@aviv.sh>
2026-04-02 16:46:30 -0400

    fixup!

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,62 +1,7 @@
 import Footer from '@node-core/ui-components/Containers/Footer';
 import NavItem from '@node-core/ui-components/Containers/NavBar/NavItem';
 
-const SOCIAL_LINKS = [
-  { icon: 'github', link: 'https://github.com/nodejs/node', alt: 'GitHub' },
-  { icon: 'discord', link: 'https://discord.gg/nodejs', alt: 'Discord' },
-  { icon: 'mastodon', link: 'https://social.lfx.dev/@nodejs', alt: 'Mastodon' },
-  {
-    icon: 'bluesky',
-    link: 'https://bsky.app/profile/nodejs.org',
-    alt: 'Bluesky',
-  },
-  { icon: 'twitter', link: 'https://twitter.com/nodejs', alt: 'Twitter' },
-  { icon: 'slack', link: 'https://slack-invite.openjsf.org/', alt: 'Slack' },
-  {
-    icon: 'linkedin',
-    link: 'https://www.linkedin.com/company/node-js',
-    alt: 'LinkedIn',
-  },
-];
-
-const FOOTER_LINKS = [
-  {
-    link: 'https://openjsf.org/',
-    text: 'OpenJS Foundation',
-  },
-  {
-    link: 'https://terms-of-use.openjsf.org/',
-    text: 'Terms of Use',
-  },
-  {
-    link: 'https://privacy-policy.openjsf.org/',
-    text: 'Privacy Policy',
-  },
-  {
-    link: 'https://bylaws.openjsf.org/',
-    text: 'Bylaws',
-  },
-  {
-    link: 'https://github.com/openjs-foundation/cross-project-council/blob/main/CODE_OF_CONDUCT.md',
-    text: 'Code of Conduct',
-  },
-  {
-    link: 'https://trademark-policy.openjsf.org/',
-    text: 'Trademark Policy',
-  },
-  {
-    link: 'https://trademark-list.openjsf.org/',
-    text: 'Trademark List',
-  },
-  {
-    link: 'https://www.linuxfoundation.org/cookies/',
-    text: 'Cookie Policy',
-  },
-  {
-    link: 'https://github.com/nodejs/node/security/policy',
-    text: 'Security Policy',
-  },
-];
+import { socialLinks, footerLinks } from './footer.json';
 
 // The Node.js Project is legally obligated to include the following text.
 // It should not be modified unless explicitly requested by OpenJS staff.
@@ -79,7 +24,7 @@ const LegalSlot = (
     </p>
 
     <p>
-      {FOOTER_LINKS.map(({ link, text }) => (
+      {footerLinks.map(({ link, text }) => (
         <NavItem key={link} type="footer" href={link}>
           {text}
         </NavItem>
@@ -94,7 +39,7 @@ const LegalSlot = (
 export default ({ metadata }) => (
   <Footer
     pathname={metadata.path}
-    navigation={{ socialLinks: SOCIAL_LINKS }}
+    navigation={{ socialLinks }}
     slots={{ legal: LegalSlot }}
   />
 );
