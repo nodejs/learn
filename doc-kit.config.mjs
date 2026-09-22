14b94e0bb56a0d3053819df08aaadb1973278b14
Jonas Geiler <git@jonasgeiler.com>
2026-09-22 23:25:13 +0200

    fix: remove `https://` prefix from `origin` to fix production build
    Signed-off-by: Jonas Geiler <git@jonasgeiler.com>

--- doc-kit.config.mjs
+++ doc-kit.config.mjs
@@ -2,10 +2,10 @@ import { join } from 'node:path';
 
 const origin =
   process.env.VERCEL_ENV === 'preview'
-    ? `https://${process.env.VERCEL_URL}`
+    ? process.env.VERCEL_URL
     : process.env.VERCEL_ENV === 'production'
-      ? 'https://nodejs.org'
-      : 'http://localhost:3000';
+      ? 'nodejs.org'
+      : 'localhost:3000';
 
 /** @type {import('@doc-kit/core/utils/configuration/types.d.ts').Configuration} */
 export default {
